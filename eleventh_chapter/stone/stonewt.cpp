d2e137a5a3c69ac25321414c579c3f3ed9e36e51
Patricxian <lp10204@163.com>
2019-12-25 22:39:27 +0800

    eleventh_chapter no stone.cpp

---
+++ eleventh_chapter/stone/stonewt.cpp
@@ -0,0 +1,35 @@
+// stonewt.cpp -- Stonewt methods
+#include <iostream>
+#include "stonewt.h"
+using std::cout;
+
+Stonewt::Stonewt(double lbs)
+{
+	stone = int(lbs) / Lbs_per_stn;
+	pds_left = int(lbs) % Lbs_per_stn + lbs - int(lbs);
+	pounds = lbs;
+}
+
+Stonewt::Stonewt(int stn, double lbs)
+{
+	stone = stn;
+	pds_left = lbs;
+	pounds = stn * Lbs_per_stn + lbs;
+}
+
+Stonewt::Stonewt()
+{
+	stone = pds_left = pounds = 0;
+}
+
+Stonewt::~Stonewt() {}
+
+void Stonewt::show_lbs()
+{
+	cout << stone << " stone, " << pds_left << " pounds\n";
+}
+
+void Stonewt::show_stn()
+{
+	cout << pounds << " pounds\n"; 
+}
