d78d0f4400c7cc19af7380316f789a41b67fad88
Patricxian <lp10204@163.com>
2020-03-17 20:04:36 +0800

    add use_stui project

---
+++ fourteenth_chapter/use_stui/studenti.cpp
@@ -0,0 +1,67 @@
+// studenti.cpp -- Student class using private inheritance
+#include "studenti.h"
+using std::ostream;
+using std::endl;
+using std::istream;
+using std::string;
+
+double Student::Average() const
+{
+	if (ArrayDb::size() > 0)
+		return ArrayDb::sum() / ArrayDb::size();
+	else
+		return 0;
+}
+
+double & Student::operator[](int i)
+{
+	return ArrayDb::operator[](i);
+}
+
+double Student::operator[](int i) const
+{
+	return ArrayDb::operator[](i);
+}
+
+const string & Student::Name() const
+{
+	return (const string &) *this;
+}
+
+ostream & Student::arr_out(ostream & os) const
+{
+	int i;
+	int lim = ArrayDb::size();
+	if (lim > 0)
+	{
+		for (i = 0; i < lim; i++)
+		{
+			os << ArrayDb::operator[](i) << " ";
+			if (i % 5 == 4)
+				os << endl;
+		}
+		if (i % 5 != 0)
+			os << endl;
+	}
+	else
+		os << " empty array ";
+	return os;
+}
+
+istream & operator >> (istream & is, Student & stu)
+{
+	is >> (string &)stu;
+}
+
+istream & getline(istream & is, Student & stu)
+{
+	getline(is, (string &)stu);
+	return is;
+}
+
+ostream & operator<<(ostream & os, const Student & stu)
+{
+	os << "Scores for " << (const string &)stu << ":\n";
+	stu.arr_out(os);
+	return os;
+}
